7c051dc4c881cba2661ffe42161f0c15d7c6dffa
Derek Silva <dereksilvamusic@gmail.com>
2018-05-12 02:18:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function dwarfRollCall(dwarves) {
+  var newStr = "";
+  dwarves.forEach(function(name, i) {
+  	newStr += (i+1 + ". " + name + " ");
+  });
+  return newStr;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+function summonCaptainPlanet(array) {
+	return array.map(function(elements) {
+		return elements.toUpperCase() + "!";
+	});
 }
 
 function longPlaneteerCalls(words) {
+	for(var i = 0; i < words.length; i++){ 
+	if(words[i].length > 4) 
+	return true;
+	}
+	return false;
 }
 
-function findTheCheese (foods) {
+function findTheCheese(foods) {
+	var cheese = ["camembert", "cheddar", "gouda"];
+	for(var i = 0; i < foods.length; i++) {
+	var cheeseI = cheese.indexOf(foods[i]);
+	if(cheeseI !== -1) {
+	return foods[i];}
+	}
+	return "no cheese!";
 }
